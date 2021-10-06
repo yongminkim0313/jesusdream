29bb245e2fbe11e4a783bd1c8d31b9a03aee08d8
kim yongmin <kimyongmin1@naver.com>
2021-10-06 10:22:51 +0900

    ++++

--- front/vue.config.js
+++ front/vue.config.js
@@ -3,11 +3,9 @@ module.exports = {
         'vuetify'
     ],
     devServer: {
-        devServer: {
-            host: '0.0.0.0',
-            https: false,
-            port: 8000,
-            public: 'http://0.0.0.0:8000'
-        },
+        host: '0.0.0.0',
+        https: false,
+        port: 8080,
+        public: 'http://0.0.0.0:8080'
     },
 }
