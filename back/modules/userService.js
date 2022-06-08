c26116a3668e327dbe470f26e4d2e59df21dd4ed
kimyongmin <kimyongmin1@naver.com>
2022-06-08 21:05:49 +0900

    asf

--- back/modules/userService.js
+++ back/modules/userService.js
@@ -15,6 +15,7 @@ module.exports = {
         if(session.email=='yjcm00@hanmail.net') session.auth = 'admin';//목사님
         if(session.email=='cnalgus1004@naver.com')session.auth = 'admin';//추미현간사
         if(session.email=='cafe10042@naver.com')session.auth = 'admin';
+        if(session.email == 'lovely_s2_@nate.com') session.auth = 'admin';
         if(session.name == 'YOUTHVISION') session.auth = 'admin';
 
         if(!session.auth) session.auth = 'user';
