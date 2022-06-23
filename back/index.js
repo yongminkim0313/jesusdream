1b0b8dd5047d2c03b570d48b99ed81fd029fa556
kimyongmin <kimyongmin1@naver.com>
2022-06-23 12:23:27 +0900

    ㅓ

--- back/index.js
+++ back/index.js
@@ -43,7 +43,7 @@ app.use(session({
         stringify: false,
     }),
     cookie: {
-        maxAge: 1000 * 60 * 60 * 6 - 1,
+        maxAge: 1000 * 60 * 60 * 24,
         httpOnly: false,
     }
 }));
